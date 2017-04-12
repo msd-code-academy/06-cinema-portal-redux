9d365ebbea6a6f7929a150c9685b2531f6475ac4
Pavel Gavlík <pavel.gavlik@merck.com>
2017-04-12 17:37:28 +0200

    Simplify login reducer tests

--- src/reducers/login.test.js
+++ src/reducers/login.test.js
@@ -1,81 +1,65 @@
-import rootReducer from './rootReducer';
+import loginReducer from './login';
 
 describe('reducers', function () {
   describe('userIdentity', function () {
     it('LOGIN_SUCCESS', () => {
-      const state = rootReducer({
-        login: {
-          user: null,
-          showLoginForm: true,
-          loginError: ''
-        }
+      const state = loginReducer({
+        user: null,
+        showLoginForm: true,
+        loginError: ''
       }, {
         type: 'LOGIN_SUCCESS',
         user: {username: 'john', password: 'abc'}
       });
       expect(state).toEqual({
-        login: {
-          loginError: '',
-          showLoginForm: false,
-          user: {
-            username: 'john',
-            password: 'abc'
-          }
+        loginError: '',
+        showLoginForm: false,
+        user: {
+          username: 'john',
+          password: 'abc'
         }
       });
     });
 
     it('LOGIN_FAIL', function () {
-      const state = rootReducer({
-        login: {
-          user: null,
-          showLoginForm: true,
-          loginError: ''
-        }
+      const state = loginReducer({
+        user: null,
+        showLoginForm: true,
+        loginError: ''
       }, {
         type: 'LOGIN_FAIL',
         error: {}
       });
       expect(state).toEqual({
-        login: {
-          user: null,
-          showLoginForm: true,
-          loginError: 'Login attempt failed.'
-        }
+        user: null,
+        showLoginForm: true,
+        loginError: 'Login attempt failed.'
       });
     });
 
     it('SHOW_LOGIN_FORM', function () {
-      const state = rootReducer({
-        login: {
-          user: null,
-          showLoginForm: false,
-          loginError: ''
-        }
+      const state = loginReducer({
+        user: null,
+        showLoginForm: false,
+        loginError: ''
       }, {type: 'SHOW_LOGIN_FORM'});
       expect(state).toEqual({
-        login: {
-          user: null,
-          showLoginForm: true,
-          loginError: ''
-        }
+        user: null,
+        showLoginForm: true,
+        loginError: ''
       });
     });
 
     it('CLOSE_LOGIN_FORM', function () {
-      const state = rootReducer({
-        login: {
-          user: null,
-          showLoginForm: true,
-          loginError: 'Login attempt failed.'
-        }
+      const state = loginReducer({
+        user: null,
+        showLoginForm: true,
+        loginError: 'Login attempt failed.'
       }, {type: 'CLOSE_LOGIN_FORM'});
       expect(state).toEqual({
-        login: {
-          user: null,
-          showLoginForm: false,
-          loginError: ''
-        }
+        user: null,
+        showLoginForm: false,
+        loginError: ''
       });
     });
   });
